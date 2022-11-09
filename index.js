b29a9475b46486b917a1125ca74abb3a32d3cb1c
Misha Hitko <109796168+MishaHitko@users.noreply.github.com>
2022-11-09 16:20:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,90 +1,56 @@
-const numbers = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
-const positiveNum = numbers.reduce((acc,number) => {
-    if (number > 0) {
-        acc.num += 1;
-        acc.sum = acc.sum + number;
+/**
+ *
+ * @param {string} name
+ * @param {number} age
+ * @constructor
+ */
+function Human(name, age) {
+    this.name = name;
+    this.age = age;
+    this.ownerCar = [];
+    this.getHumanInfo = function () {
+        console.log(this.name, this.age);
     }
-    return acc;
-}, {sum: 0, num: 0});
-console.log('Сумма позитивных елементов:', positiveNum.sum, 'Количество позитивных елементов:', positiveNum.num);
+}
+const john = new Human('John',  25);
+john.getHumanInfo();
+const jo = new Human('Jo',  30);
+jo.getHumanInfo();
+const olha = new Human('Olha',  20);
+olha.getHumanInfo();
 
-const minNum = numbers.reduce((acc, number, index) => {
-    if (acc.num > number) {
-        acc.num = number;
-        acc.index = index;
+/**
+ *
+ * @param {string} brand
+ * @param {string} model
+ * @param {number} yearOfIssue
+ * @param {string} plateNumber
+ * @constructor
+ */
+function Auto(brand, model, yearOfIssue, plateNumber) {
+    this.brand = brand;
+    this.model = model;
+    this.yearOfIssue = yearOfIssue;
+    this.plateNumber = plateNumber;
+    this.owner = [];
+    this.setOwner = function (human) {
+        if (human.age >= 18) {
+            this.owner.push(human);
+            human.ownerCar.push(this);
+        } else {
+            console.log(`${human} вам нет 18 лет, вы не можете быть владельцем автомобиля`)
+        }
     }
-    return acc;
-}, {num: 0, index: 0});
-console.log('Минимальное число:', minNum.num,'Индекс минимального числа:', minNum.index);
+    this.getAutoInfo = function () {
+        console.log('Авто:', this.brand, this.model, this.yearOfIssue, this.plateNumber);
+        this.owner.forEach((item) => {
+            item.getHumanInfo();
+        });
+    }
+}
 
-const maxNum = numbers.reduce((acc, number, index) => {
-    if (acc.num < number) {
-        acc.num = number;
-        acc.index = index;
-    }
-    return acc;
-}, {num: 0, index: 0});
-console.log('Максимальное число:', maxNum.num,'Индекс максимального числа:', maxNum.index);
 
-const negativeNum = numbers.reduce((acc,number) => {
-    if (number < 0) {
-        acc += 1;
-    }
-    return acc;
-}, 0);
-console.log('Количество негативных елементов:', negativeNum);
-
-const oddNum = numbers.reduce((acc,number) => {
-    if (number % 2 !== 0 && number > 0) {
-        acc += 1;
-    }
-    return acc;
-}, 0);
-console.log('Количество непарных позитивных елементов:', oddNum);
-
-const evenNum = numbers.reduce((acc,number) => {
-    if (number % 2 === 0 && number > 0) {
-        acc += 1;
-    }
-    return acc;
-}, 0);
-console.log('Количество парных позитивных елементов:',evenNum);
-
-const evenNumSum = numbers.reduce((acc,number) => {
-    if (number % 2 === 0 && number > 0) {
-        acc += number;
-    }
-    return acc;
-}, 0);
-console.log('Сумма парных позитивных елементов:',evenNumSum);
-
-const oddNumSum = numbers.reduce((acc,number) => {
-    if (number % 2 !== 0 && number > 0) {
-        acc += number;
-    }
-    return acc;
-}, 0);
-console.log('Сумма непарных позитивных елементов:',oddNumSum);
-
-const multiNumSum = numbers.reduce((acc,number) => {
-    if (number > 0) {
-        acc *= number;
-    }
-    return acc;
-}, 1);
-console.log('Умножение всех позитивных елементов:',multiNumSum);
-
-let maxNumber = 0;
-const maxNumArray = numbers.map((number) => {
-    if (number > maxNumber) {
-        maxNumber = number;
-    }
-    return number;
-}).map((number) => {
-    if (number < maxNumber) {
-        number = 0;
-    }
-    return number;
-})
-
-console.log('Найден наибольший елемент массива, остальные обнулены:',maxNumArray);
+const bmw = new Auto('bmw', 'BMW X6', 2020, 'KA5242XI');
+bmw.setOwner(john);
+bmw.setOwner(jo);
+bmw.getAutoInfo();
